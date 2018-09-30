05c433fac9852e2a0f67082ebccb087abc0629e8
Natalia <natioliveira97@hotmail.com>
2018-09-30 11:33:44 -0300

    ajustado para a versao

--- src/CMakeFiles/pd3.dir/cmake_clean.cmake
+++ src/CMakeFiles/pd3.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/pd3.dir/main.cpp.o"
   "pd3.pdb"
   "pd3"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/pd3.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/pd3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

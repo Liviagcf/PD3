4e67beedd39e2308fd2c251d3f8bf5ccfcbbe570
Natalia <natioliveira97@hotmail.com>
2018-10-06 23:06:33 -0300

    retificacao por epilines

--- src/main.cpp
+++ src/main.cpp
@@ -22,16 +22,16 @@ void mouseClick(int event, int x, int y, int flags, void* userdata){
     Controller *controller = (Controller*)userdata;
 
     if  ( event == EVENT_LBUTTONDOWN ){
-    	if(controller->clicks < 10){
+    	if(controller->clicks < 30){
     		Point2f point = Point2f(x,y);
     		controller->points.push_back(point);
     		++controller->clicks;
     	}
+    	//cout << x << ' ' << y << endl;
     	cout << controller->windowsName << " " << controller->points.size() << endl;
     }
 
 }
-
 
 
 vector<float> getWorldCoordinates(float xl, float xr, float yl, float yr){
@@ -123,15 +123,15 @@ void createDisparity(){
 	int speckleRange = 20;
 	double min, max;
 
-
-	//Rview = imread("Limage.jpg");
-	//Lview = imread("Rimage.jpg");
-
 	cvtColor(Lview, Lview,COLOR_RGB2GRAY, 0);
 	cvtColor(Rview, Rview,COLOR_RGB2GRAY, 0);
 
+	cout << "1" << endl;
+
 	Ptr<StereoSGBM> stereo_sgbm = StereoSGBM::create(minDisparity, numDisparities, SADWindowSize, P1, P2, disp12MaxDiff, preFilterCap, uniquenessRatio, speckleWindowSize, speckleRange, false);
 	stereo_sgbm->compute(Lview,Rview,disp);
+
+
 
 	// A saida do disparity map é uma matriz em que cada elemento possui 16bits sinalizados, sendo os 4 ultimos fracionais
 	// Vamos ignorar os bits fracionais.
@@ -161,7 +161,7 @@ void createDisparity(){
 	destroyAllWindows();
 }
 
-void requisito2(){
+void stereoRetification(){
 	Controller Lcamera, Rcamera;
 
 	//Inicializando os dados
@@ -179,7 +179,7 @@ void requisito2(){
     double data1[9] = {6704.926882, 0.000103, 738.251932, 0, 6705.241311, 457.560286, 0, 0, 1};
     double data2[9] = {6682.125964, 0.000101, 875.207200, 0, 6681.475962, 357.700292, 0, 0, 1};
     double data3[9] = {0.70717199,  0.70613396, -0.03581348, 0.28815232, -0.33409066, -0.89741388 ,-0.64565936,  0.62430623, -0.43973369};
-    double data4[9] = {0.48946344,  0.87099159, -0.04241701 ,0.33782142, -0.23423702, -0.91159734 ,-0.80392924,  0.43186419, -0.40889007};
+    double data4[9] = {0.48946344,  0.87099159, -0.04241701, 0.33782142, -0.23423702, -0.91159734 ,-0.80392924,  0.43186419, -0.40889007};
     double data5[3] = {-532.285900 , 207.183600 , 2977.408000};
     double data6[3] = {-614.549000 , 193.240700 , 3242.754000}; 
 
@@ -191,15 +191,15 @@ void requisito2(){
     Rcamera.rotation  = Mat(3,3,CV_64FC1, data4);
 
     Lcamera.translation = Mat(3,1,CV_64FC1, data5);
-    Rcamera.translation =Mat(3,1,CV_64FC1, data6);
+    Rcamera.translation = Mat(3,1,CV_64FC1, data6);
 
-    Mat Linverse;
-	invert(Lcamera.rotation, Linverse, DECOMP_LU);
+    Mat Rtranspost;
+	transpose(Rcamera.rotation, Rtranspost);
 
     //Rotacao da camera da direita em relacao a camera da esquerda  R = Rl.inv * Rr,
  	//Translacao da camera da direita em relacao a da esquerda T = Rl.inv * (tr - tl).
-    Mat R = Linverse*Rcamera.rotation;
-    Mat T = Linverse*(Rcamera.translation - Lcamera.translation);
+    Mat R = Rtranspost*Lcamera.rotation;
+    Mat T = Rcamera.translation - (R*Lcamera.translation);
     
     namedWindow(Lcamera.windowsName, WINDOW_NORMAL);
 	imshow(Lcamera.windowsName, Lcamera.image);
@@ -207,15 +207,13 @@ void requisito2(){
 	namedWindow(Rcamera.windowsName, WINDOW_NORMAL);
 	imshow(Rcamera.windowsName, Rcamera.image);
 
-	//setMouseCallback(Lcamera.windowsName, mouseClick, &Lcamera);
+	setMouseCallback(Lcamera.windowsName, mouseClick, &Lcamera);
 
-	//setMouseCallback(Rcamera.windowsName, mouseClick, &Rcamera);
+	setMouseCallback(Rcamera.windowsName, mouseClick, &Rcamera);
 
-	waitKey(0);
-
-   /* while(Lcamera.points.size() < 1 || Rcamera.points.size() < 1){
+    while(Lcamera.points.size() < 10 || Rcamera.points.size() < 10){
    		waitKey(100);
-	}*/
+	}
 
 	cout<<"saiu"<<endl;
 
@@ -226,10 +224,10 @@ void requisito2(){
 
 	Mat R1, R2, P1, P2, Q, LMap1, LMap2, RMap1, RMap2, Lout, Rout;
 
-	stereoRectify(Lcamera.intrinsics, Lcamera.distcoef, Rcamera.intrinsics, Rcamera.distcoef, Lcamera.image.size(), R, T, R1, R2, P1, P2, Q, CALIB_ZERO_DISPARITY, -1, Lcamera.image.size(), 0, 0 );
+	stereoRectify(Rcamera.intrinsics, Rcamera.distcoef, Lcamera.intrinsics, Lcamera.distcoef, Lcamera.image.size(), R, T, R1, R2, P1, P2, Q, CALIB_ZERO_DISPARITY, -1, Lcamera.image.size(), 0,0 );
 
-	initUndistortRectifyMap(Lcamera.intrinsics, Lcamera.distcoef, R1, P1, Lcamera.image.size(),CV_32FC1, LMap1, LMap2);
-	initUndistortRectifyMap(Rcamera.intrinsics, Rcamera.distcoef, R2, P2, Lcamera.image.size(),CV_32FC1, RMap1, RMap2);
+	initUndistortRectifyMap(Lcamera.intrinsics, Lcamera.distcoef, R2, P2, Lcamera.image.size(),CV_32FC1, LMap1, LMap2);
+	initUndistortRectifyMap(Rcamera.intrinsics, Rcamera.distcoef, R1, P1, Lcamera.image.size(),CV_32FC1, RMap1, RMap2);
 
 	remap(Lcamera.image, Lout, LMap1, LMap2, INTER_LINEAR, BORDER_CONSTANT, 0);
 	remap(Rcamera.image, Rout, RMap1, RMap2, INTER_LINEAR, BORDER_CONSTANT, 0);
@@ -237,58 +235,101 @@ void requisito2(){
 	imshow(Lcamera.windowsName, Lout);
 	imshow(Rcamera.windowsName, Rout);
 
+	Rview = Rcamera.image;
+	Lview = Lout;
+
+	//createDisparity();
+
 	waitKey(0);
+}
 
+void uncalibratedRetification(){
+	Controller Lcamera, Rcamera;
 
+	//Inicializando os dados
+	Lcamera.image = imread("../data/MorpheusL.jpg");
+	Rcamera.image = imread("../data/MorpheusR.jpg");
 
+	if(!Lcamera.image.data || !Rcamera.image.data){
+        cout << endl << "Nao foi possivel abrir imagem ou ela não está nessa pasta!" << endl;
+        while (cin.get() != '\n');
+        return;
+    }
+    Lcamera.windowsName = "Lcamera";
+    Rcamera.windowsName = "Rcamera";
 
-/*
-	//Calcula epilines (funciona parcialmente)
-	vector<cv::Vec3f> lines1, lines2;
-
-	Mat F = findFundamentalMat(Lcamera.points, Rcamera.points, FM_RANSAC, 3, 0.99);
-
-	Mat H1, H2;
-
-	//H1 = findHomography(Lcamera.points, Rcamera.points, CV_RANSAC, 3);
-	stereoRectifyUncalibrated(Lcamera.points, Rcamera.points, F, Lcamera.image.size(), H1, H2, 5);
-
-	Mat Ldst, Rdst;
-
-	warpPerspective(Rcamera.image, Rdst, H1, Rcamera.image.size(), INTER_LINEAR , BORDER_TRANSPARENT);
-	//warpPerspective(Lcamera.image, Ldst, H2, Rcamera.image.size());
-
-	computeCorrespondEpilines(Rcamera.points, 1, F, lines1);
-	computeCorrespondEpilines(Lcamera.points, 2, F, lines2);
-
-
-	Scalar color(0,0,255);
- 	for(int i=0; i<lines1.size(); i++){
-	    line(Lcamera.image,Point(0,-lines1[i][2]/lines1[i][1]),Point(Lcamera.image.cols,-(lines1[i][2]+lines1[i][0]*Lcamera.image.cols)/lines1[i][1]),color);
-
-	 	line(Rcamera.image,Point(0,-lines2[i][2]/lines2[i][1]),Point(Rcamera.image.cols,-(lines2[i][2]+lines2[i][0]*Rcamera.image.cols)/lines2[i][1]),color);
-	}
-
+    resize(Rcamera.image, Rcamera.image, Lcamera.image.size(),6682.125964*0.000101 ,6681.475962*0.000101, INTER_LINEAR);
 
 	namedWindow(Lcamera.windowsName, WINDOW_NORMAL);
 	imshow(Lcamera.windowsName, Lcamera.image);
 
 	namedWindow(Rcamera.windowsName, WINDOW_NORMAL);
-	imshow(Rcamera.windowsName, Rdst);
+	imshow(Rcamera.windowsName, Rcamera.image);
 
-	imwrite("Limage.jpg", Lcamera.image);
-	imwrite("Rimage.jpg", Rdst);
+	setMouseCallback(Lcamera.windowsName, mouseClick, &Lcamera);
 
-	cout << Lcamera.image.size() << " " << Rdst.size() << " " << Rcamera.image.size() <<endl;
+	setMouseCallback(Rcamera.windowsName, mouseClick, &Rcamera);
 
-	Lview=Lcamera.image;
-	Rview=Rdst;
+    while(Lcamera.points.size() < 15 || Rcamera.points.size() < 15){
+   		waitKey(100);
+	}
+
+	cout<<"saiu"<<endl;
+
+	//Imagens precisam ter o mesmo tamanho
+	imshow(Rcamera.windowsName, Rcamera.image);
+	waitKey(0);
+
+
+	//Calcula epilines (funciona parcialmente) 
+	vector<cv::Vec3f> lines1, lines2;
+
+	//Mat F1 = findFundamentalMat(Lcamera.points, Rcamera.points, FM_RANSAC, 3, 0.99);
+	//cout << F1.size() << endl;
+	Mat F2 = findFundamentalMat(Rcamera.points, Lcamera.points, FM_RANSAC, 3, 0.99);
+	//cout << F2.size() << endl;
+
+	Mat LH1, LH2, RH1, RH2;
+
+	//stereoRectifyUncalibrated(Lcamera.points, Rcamera.points, F1, Lcamera.image.size(), LH1, LH2, 5);
+	stereoRectifyUncalibrated(Rcamera.points, Lcamera.points, F2, Lcamera.image.size(), RH1, RH2, 5);
+
+	Mat Ldst, Rdst;
+
+	computeCorrespondEpilines(Rcamera.points, 1, F2, lines1);
+	computeCorrespondEpilines(Lcamera.points, 2, F2, lines2);
+
+	Rdst = Rcamera.image.clone();
+	Ldst = Lcamera.image.clone();
+
+ 	for(int i=0; i<lines1.size(); i++){
+ 		Scalar color(rand() % 255,rand() % 255,rand() % 255);  
+	    line(Ldst, Point(0,-lines1[i][2]/lines1[i][1]), Point(Lcamera.image.cols,-(lines1[i][2]+lines1[i][0]*Lcamera.image.cols)/lines1[i][1]),color, 3,8,0);
+	 	line(Rdst,Point(0, -lines2[i][2]/lines2[i][1]), Point(Rcamera.image.cols,-(lines2[i][2]+lines2[i][0]*Rcamera.image.cols)/lines2[i][1]),color,3,8,0);	
+		circle(Ldst, Point(Lcamera.points[i][0],Lcamera.points[i][1]), 15, color, 10, 8, 0);
+		circle(Rdst, Point(Rcamera.points[i][0],Rcamera.points[i][1]), 15, color, 10, 8, 0);
+	}
+
+	warpPerspective(Rcamera.image, Rcamera.image, RH1, Rcamera.image.size(), INTER_LINEAR , BORDER_TRANSPARENT);
+	warpPerspective(Lcamera.image, Lcamera.image, RH2, Rcamera.image.size());
+
+	namedWindow(Lcamera.windowsName, WINDOW_NORMAL);
+	imshow(Lcamera.windowsName, Lcamera.image);
+	namedWindow(Rcamera.windowsName, WINDOW_NORMAL);
+	imshow(Rcamera.windowsName, Rcamera.image);
+
+	namedWindow("oi", WINDOW_NORMAL);
+	imshow("oi", Rdst);
+	namedWindow("tchau", WINDOW_NORMAL);
+	imshow("tchau", Ldst);
+
+	imwrite("tentL.png", Lcamera.image);
+	imwrite("tentR.png", Rcamera.image);
+
 
 
 	waitKey(0);
-	//createDisparity();*/
 }
-
 
 Mat EncontraCorrelacao(Mat imgL, Mat disp){
 
@@ -303,7 +344,6 @@ Mat EncontraCorrelacao(Mat imgL, Mat disp){
 	return corr;
 
 }
-
 
 //encontra o maior valor de distância e transforma pra 255, e os outros valores ficam proporcionais a isso
 void encontraMaioreTransforma(Mat* image){
@@ -328,12 +368,15 @@ void encontraMaioreTransforma(Mat* image){
 }
 
 void requisito1(){
+	Lview = imread("tentL.png");
+	Rview= imread("tentR.png");
+
 	createDisparity();
 	createDepth();
 }
 
 
 int main(){
-	requisito2();
+	requisito1();
 }
 
